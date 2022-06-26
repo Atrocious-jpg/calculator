7d9207b22619ac54e82713b8acb985724c8cd920
John Muinde <jhnmuinde@gmail.com>
2022-06-26 19:19:57 +0300

    First calc commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-// *** Write your code here***
+// *** Write your code here**
+    let dResult = document.querySelector('[name ="displayResult"]')
+     
+function calcNumbers(val) {
+        dResult.value += val;
+   } 
